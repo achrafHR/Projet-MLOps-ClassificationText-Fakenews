3f5171430589d782327e9b569065f4087eba24f1
unknown <hrouchachraf001@gmail.com>
2025-05-07 19:00:22 +0100

    data ingestion added (etape de telechergement de dataset)

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from fakeNewsClassifier import logger
+from fakeNewsClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to fakeNewsClassifier")
+STAGE_NAME = "Data Ingestion Stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
